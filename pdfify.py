cd75f46a5037cd4363e16b983356bdc5ff2cfde2
William Panlener <wpanlener@gmail.com>
2020-06-26 20:18:35 -0500

    Render a cover, reduce outline depth, and upload .config/computerScience2020.toc

--- pdfify.py
+++ pdfify.py
@@ -3,6 +3,8 @@
 import os
 import pdfkit
 
+from jinja2 import Environment, FileSystemLoader
+
 print(f'cwd={os.getcwd()}')
 
 def remove_prefix(text, prefix):
@@ -12,6 +14,9 @@ def remove_prefix(text, prefix):
 
 if not os.path.exists('.config'):
     os.makedirs('.config')
+
+env = Environment(loader=FileSystemLoader('templates'))
+template = env.get_template('document.j2')
 
 files = []
 
@@ -30,12 +35,32 @@ if not os.path.exists('.config/corpus.toc'):
 
 
 # Build file list from TOC
-with open('.config/engineering-sample.toc') as f:
+with open('.config/computerScience-2020.toc') as f:
     files = f.readlines()
 
 # Remove newline characters from filenames
 files = [l.strip('\n\r') for l in files]
 
+# Render cover
+with open('.cache/cover.html', 'w') as f:
+    f.write(template.render(
+        main_content=""""
+        <h1>University of Mississippi<br>
+        School of Engineering<br>
+        2020 Catalog</h1>
+        """
+    ))
+
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': "UTF-8",
+    'outline-depth': 2
+}
+
 print('Generating document catalog.pdf. This will take on the order of minutes.')
 os.chdir('.cache/catalog.olemiss.edu')
-pdfkit.from_file(files, '../../catalog.pdf')
+pdfkit.from_file(files, '../../catalog.pdf', options=options, cover='../cover.html')
